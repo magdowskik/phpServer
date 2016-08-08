087fcef4d728f40112ca44d4ed5389bbbda95982
Krzysztof Magdowski <krzysztof.magdowski@ttms.pl>
2016-08-08 11:01:12 +0200

    no message

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 	
 	echo "Database connection test";
 	$dbconn = pg_connect("host=ec2-54-235-254-199.compute-1.amazonaws.com port=5432 dbname=dbih69tnc5onrf user=lugudsfmtmquvy password=wUsYz-VDPWYn5BEPoIhCX_IO2l") or die('connection failed');
-	$result2 = pg_query($dbconn, "INSERT INTO newTable(ID,NAME,SURNAME) VALUES(3,'assd','qswe');");
-	var_dump(pg_fetch_all($result2));
 	$result3 = pg_query($dbconn, "select * from newTable;");
 	var_dump(pg_fetch_all($result3));
-	foreach($result3 as $result) {
+	foreach(pg_fetch_all($result3) as $result) {
 		echo $result;
 	}
 	
-	foreach($result3 as $result) {
+	foreach(pg_fetch_all($result3) as $result) {
 		echo $result["name"];
 	}
 	
