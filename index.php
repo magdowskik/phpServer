139daab10d147132bea2989e6554bf080f610ca9
magdowskik <krzysztof.magdowski@ttms.pl>
2016-08-25 14:24:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,59 @@
 <?php
 	
-	echo "Database connection test";
-	$dbconn = pg_connect("host=ec2-54-235-254-199.compute-1.amazonaws.com port=5432 dbname=dbih69tnc5onrf user=lugudsfmtmquvy password=wUsYz-VDPWYn5BEPoIhCX_IO2l") or die('connection failed');
-	$result3 = pg_query($dbconn, "select * from newTable;");
-	var_dump(pg_fetch_all($result3));
-	foreach(pg_fetch_all($result3) as $result) {
-		echo $result;
-	}
+	// echo "Database connection test";
+	// $dbconn = pg_connect("host=ec2-54-235-254-199.compute-1.amazonaws.com port=5432 dbname=dbih69tnc5onrf user=lugudsfmtmquvy password=wUsYz-VDPWYn5BEPoIhCX_IO2l") or die('connection failed');
+	// $result3 = pg_query($dbconn, "select * from newTable;");
+	// var_dump(pg_fetch_all($result3));
+	// foreach(pg_fetch_all($result3) as $result) {
+	// 	echo $result;
+	// }
 	
-	foreach(pg_fetch_all($result3) as $result) {
-		echo $result["name"];
-	}
+	// foreach(pg_fetch_all($result3) as $result) {
+	// 	echo $result["name"];
+	// }
+	<?php
+// multiple recipients
+$to  = 'krzysztmagda@gmail.com'; //. ', '; // note the comma
+// $to .= 'wez@example.com';
+
+// subject
+$subject = 'Birthday Reminders for August';
+
+// message
+$message = '
+<html>
+<head>
+  <title>Birthday Reminders for August</title>
+</head>
+<body>
+  <p>Here are the birthdays upcoming in August!</p>
+  <table>
+    <tr>
+      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+    </tr>
+    <tr>
+      <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Additional headers
+$headers .= 'To: Mary <krzysztmagda@gmail.com>' . "\r\n";
+$headers .= 'From: Birthday Reminder <birthday@example.com>' . "\r\n";
+$headers .= 'Cc: birthdayarchive@example.com' . "\r\n";
+$headers .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+
+// Mail it
+mail($to, $subject, $message, $headers);
+?>
 	
 ?>
