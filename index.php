9638f50c74be11c8eccea34535f06e044626d481
krzys <krzysztof.magdowski@ttms.pl>
2017-06-26 11:14:33 +0200

    2

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	define("USERNAME", "krzysztof.magdowski@ttms.akademia.lipiec.2016"); 
 	define("PASSWORD", "8uhb*UHB"); 
 	define("SECURITY_TOKEN", "AqvMzXHKIPMbpmeJaX0bkqmGt"); 
+	
 	include('soapclient/SforceEnterpriseClient.php'); 
 	require_once("soapclient/SforceHeaderOptions.php"); 
 	
@@ -9,13 +10,12 @@
 	$mySforceConnection->createConnection("soapclient/wsdl.wsdl.xml"); 
 	$mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
 	
-	$query = "SELECT Id, FirstName, LastName from Contact"; 
+	$query = "SELECT Id, Name from Account"; 
 	$response = $mySforceConnection->query($query); 
 	// QueryResult object is only for PARTNER client $queryResult = new QueryResult($response); 
-	foreach ($queryResult->records as $record){ 	
+	foreach ($response->records as $record){ 	
 		echo "Id = ".$record->Id; 
-		echo "First Name = ".$record->FirstName; 
-		echo "Last Name = ".$record->LastName; 
+		echo "First Name = ".$record->Name; 
 	}
 ?>
 
